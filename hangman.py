97a142c1ff40871fb0a359ac3be964384d854b91
Zachary Donnelly <zacharydonnelly_IT@protonmail.com>
2023-07-20 21:45:01 -0600

    finalized game and README files

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@
 import random
 from time import sleep
 
-
+#stages of hangman
 hangman_status = ['''
                +---+
                |   |
@@ -127,7 +127,7 @@ losing_message = '''\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
 '''
 
 
-
+#list store the names that are in local files
 animal_names = []
 food_names = []
 plant_names = []
@@ -143,7 +143,13 @@ game_lost_count = 0
 
 
 def game_stats():
+    '''
+    When run it will display the game information
+    whether or not the play won or lost which will
+    provide the secret word and game statistics
+    '''
     global secret_word
+
     return f'''
 ##################################################################
                          
@@ -158,6 +164,12 @@ def game_stats():
 
 
 def convert_list_strings(list):
+    '''
+    This function converts the blank_word_list
+    as a string which allows game to display
+    the hidden word in a clean appearance when
+    printed to terminal.
+    '''
     converted_list = ""
 
     for letters in list:
@@ -167,7 +179,10 @@ def convert_list_strings(list):
 
 
 def game_restart():
-
+    '''
+    Prompts user with the ability to replay game 
+    or exit the terminal.
+    '''
     new_game = input("Would you like to a new game? yes or no: ")
     if new_game == 'yes':
         game_intialization()
@@ -176,6 +191,12 @@ def game_restart():
 
 
 def is_attempts_over(guess_count):
+    '''
+    Checks whether or not the guess count is 
+    maxed out based on the guess count but
+    which is tied to the amount body parts
+    on the well hanging man...
+    '''
     global game_lost_count
 
     if guess_count == 6:
@@ -187,10 +208,19 @@ def is_attempts_over(guess_count):
     
 
 def print_hangman_status(guess_count):
+    '''
+    returns a string that is the current
+    status of the hangman character.
+    '''
     return hangman_status[guess_count]
     
 
 def is_letter_repeated(letter, word):
+    '''
+    Assesses whether or not the current letter 
+    entered is a repeated entry that the game
+    stores in already_guessed list. 
+    '''
     if letter in already_guessed:
         return True
     else:
@@ -198,7 +228,10 @@ def is_letter_repeated(letter, word):
     
 
 def is_letter_correct(letter, word):
-
+    '''
+    Assesses whether or not the letter is even
+    present in the hidden word.
+    '''
     if letter in word:
         return True
     else:
@@ -206,6 +239,24 @@ def is_letter_correct(letter, word):
     
 
 def game_logic(letter, word):
+    '''
+    Function is the primary function that is 
+    managing the game logic. It takes advantage
+    of is_letter_correct to check if the letter
+    is present in the hidden word. game_logic
+    also uses is_letter_repeated to checks if
+    the letter is a repeated entry.
+
+    If all goes well the function will address 
+    three responses:
+
+                'already guessed'
+                    'correct'
+                   'incorrect'
+    
+    If all attempted guesses are max out
+    is_attempts_over will execute as True
+    '''
     global failed_guess_count
     
     if is_letter_correct(letter, word) == True:
@@ -233,10 +284,14 @@ def game_logic(letter, word):
 
 
 def game_play(word_list):
+    '''
+    This manages the priamry user interface from
+    the questions and hangman status displayed to
+    user.
+    '''
     global game_won_count
 
     if '_' not in blank_word_list:
-        print(winning_message)
         game_won_count += 1
         print(winning_message)
         print(game_stats())
@@ -260,7 +315,11 @@ def game_play(word_list):
 
 
 def game_setup(secret_word):
-
+    '''
+    Establishes a list with correct amount of
+    blank slots for all the hidden letters of
+    the hidden "secret" word.
+    '''
     secret_word_list = list(secret_word.lower())
 
     for letters in range(len(secret_word)):
@@ -274,7 +333,12 @@ def game_setup(secret_word):
 
 #Establishes a random word for the hangman
 def game_intialization():
-
+    '''
+    This is the first function to execute which will
+    construct the introduction and ask for user to 
+    choose a category of words that will be prompted
+    later in game_play().
+    '''
     global already_guessed
     global failed_guess_count
     global blank_word_list
@@ -294,6 +358,7 @@ def game_intialization():
     user_category_choice = input("Type a category from the list above:")
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
+    #This is in-case the input is not a valid response
     try:
         with open(f"{user_category_choice}.txt", "r") as item:
             lines = item.readlines()
@@ -308,7 +373,8 @@ def game_intialization():
         print()
         game_intialization()
 
-    
+    #if statements figure strips the newlines from names stored
+    #in the name lists
     if user_category_choice == 'animals':
         for line in lines:
             animal_names.append(line.rstrip())
